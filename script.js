ea8bd42299250b97666e07995b446cb10cfca13a
Macbook Pro <macbookpro@macbookpro.home>
2025-11-26 11:41:24 +0000

    Add dynamic grid resizing with user input and square scaling

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 const gridContainer = document.getElementById('grid-container');
-const gridSize = 16;
+const GRID_SIZE = 500; 
 
-function createGrid () {
-    for (let i =0; i< gridSize * gridSize; i++) {
+function createGrid (size) {
+      
+    
+    for (let i =0; i< size * size; i++) {
         const square = document.createElement ('div');
+      
         square.className = 'grid-square';
+            const squareSize = GRID_SIZE / size;
+    square.style.width = `${squareSize}px`;
+    square.style.height = `${squareSize}px`;
 
         square.addEventListener('mouseover', () =>{
             square.style.background = 'black';
@@ -14,4 +20,23 @@ function createGrid () {
         gridContainer.appendChild(square)
     }
 }
-createGrid ();
+createGrid(16)
+
+const sizeBtn = document.querySelector('#sizeBtn')
+
+sizeBtn.addEventListener('click', () =>{
+       const input = prompt('How many squares per side? (max 100)')
+    const size = parseInt(input);
+   
+
+if (isNaN(size) || size <= 0 || size > 100) {
+  alert('Please enter a number between 1 and 100');
+  return;
+
+}
+
+
+gridContainer.innerHTML = ''
+createGrid(size)
+
+})
