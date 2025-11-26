5b61a9aed671c787ffa82b31d5dee721198e01f0
Macbook Pro <macbookpro@macbookpro.home>
2025-11-26 12:43:24 +0000

    Add random RGB color generation on hover

--- script.js
+++ script.js
@@ -13,7 +13,18 @@ function createGrid (size) {
     square.style.height = `${squareSize}px`;
 
         square.addEventListener('mouseover', () =>{
-            square.style.background = 'black';
+            function random_bg_color() {
+    var x = Math.floor(Math.random() * 256);
+    var y = Math.floor(Math.random() * 256);
+    var z = Math.floor(Math.random() * 256);
+    var bgColor = "rgb(" + x + "," + y + "," + z + ")";
+ console.log(bgColor);
+  
+    square.style.background = bgColor;
+    }
+
+random_bg_color();
+           
         })
 
 
